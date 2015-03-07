592191fc3d4a70316bc7fdc78c1763f68f8aa32e
amZotti <anthonymzotti@gmail.com>
2015-03-06 17:16:52 -0800

    User can add and shorten links

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,13 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $location, $http, Links) {
+
+  $scope.link = {};
+
+  $scope.addLink = function() {
+    console.log($location);
+    $http.post('/api/links', {url: $location}).success(function(data) {
+      $scope.link[$location] = data;
+    });
+  };
 });
